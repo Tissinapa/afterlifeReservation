d550e03979f03a70f688f1bb881155192b47d670
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2025-10-22 10:15:55 +0300

    Init

---
+++ src/main/java/com/afterlife/reservation/ReservationController.java
@@ -0,0 +1,13 @@
+package com.afterlife.reservation;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReservationController {
+    @GetMapping("/home")
+    public String Home(){
+        return "This is reservation service for afterlife";
+    }
+}
