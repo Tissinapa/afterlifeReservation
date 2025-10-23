56905d1016e121a62cd11e59a5fdc420be70f70c
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2025-10-23 18:05:28 +0300

    user getter,setter

--- src/main/java/com/afterlife/reservation/Entities/UserEntity.java
+++ src/main/java/com/afterlife/reservation/Entities/UserEntity.java
@@ -26,6 +26,52 @@ public class UserEntity {
     private List<TicketEntity> giftedTickets;
 
 
+    public UserEntity() {
+    }
+    public UserEntity(String username, String email, String passwordHash, boolean isAdmin){
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.isAdmin = isAdmin;
+        this.createdAt = LocalDateTime.now();
+    }
 
+    public Long getUserId(){
+        return userId;
+    }
+    public void setUserId(Long userId){
+        this.userId = userId;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.isAdmin = admin;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
