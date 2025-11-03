525ab56068bcce436d30a2b7f5be1876f82febb4
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2025-11-03 16:11:03 +0200

    fix cannot select seat that is already selected

--- src/main/java/com/afterlife/reservation/Repositories/SeatRepository.java
+++ src/main/java/com/afterlife/reservation/Repositories/SeatRepository.java
@@ -1,9 +1,16 @@
 package com.afterlife.reservation.Repositories;
 
+import com.afterlife.reservation.Entities.RealmType;
 import com.afterlife.reservation.Entities.SeatEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SeatRepository extends JpaRepository<SeatEntity , Long> {
+    Optional<SeatEntity> findByRealmAndSectionAndSeatNumber(
+            RealmType realmType,
+            String section,
+            String seatNumber);
 }
--- src/main/java/com/afterlife/reservation/Services/SeatServiceImpl.java
+++ src/main/java/com/afterlife/reservation/Services/SeatServiceImpl.java
@@ -21,6 +21,15 @@ public class SeatServiceImpl implements SeatService{
     @Override
     public SeatEntity reserveSeat(SeatEntity seat){
 
+        if(seatRepository.findByRealmAndSectionAndSeatNumber(
+                seat.getRealm(),
+                seat.getSection(),
+                seat.getSeatNumber()
+        ).isPresent()){
+            throw new IllegalStateException("Seat is already taken!");
+        }
+
+
         return seatRepository.save(seat);
     }
 
